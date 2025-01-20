43b525b76b74af02cc223e0ed8d092bcf6047f7c
Roma Volkov <r.volkov@yaware.com>
2025-01-20 22:38:08 +0200

    require vue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,30 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    // server: {
+    //     host: 'localhost',
+    //     port: 3000,
+    // },
+    // build: {
+    //     manifest: true,
+    //     outDir: 'public/build',
+    // },
 });
