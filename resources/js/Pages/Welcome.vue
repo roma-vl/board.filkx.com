6b1163c8696025c847d319b3a00b88ec6d33caac
Roma Volkov <Drakyla60@gmail.com>
2025-01-30 20:25:15 +0200

    template notify

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -34,8 +34,7 @@ function handleImageError() {
         <img
             id="background"
             class="absolute -left-20 top-0 max-w-[877px]"
-            src="https://laravel.com/assets/img/welcome/background.svg"
-        />
+            src="https://laravel.com/assets/img/welcome/background.svg" alt=""/>
         <div
             class="relative flex min-h-screen flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white"
         >
