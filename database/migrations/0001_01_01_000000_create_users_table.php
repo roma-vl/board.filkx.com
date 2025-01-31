9212cd6e1fe984bee22fa64c5eca81f8a8184e00
Roma Volkov <Drakyla60@gmail.com>
2025-01-31 16:44:55 +0200

    admin Users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,8 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->nullable();
             $table->string('locale')->default('uk');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
 
