4d240e6debcb806a096f8bdea44d503886816266
Roma Volkov <Drakyla60@gmail.com>
2025-05-26 13:44:32 +0300

    BA-1 #comment fix style #In Progress

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -250,12 +250,4 @@ const subCategories = computed(() => {
 </template>
 
 <style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
-}
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
 </style>
