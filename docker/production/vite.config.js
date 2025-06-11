b2ab4392b08c3028f1473b1d8f99ab3f3e77b3b6
Roma Volkov <Drakyla60@gmail.com>
2025-06-11 20:47:33 +0300

    fix docker front

--- docker/production/vite.config.js
+++ docker/production/vite.config.js
@@ -3,32 +3,32 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/js/app.js'],
-            refresh: true,
-            buildDirectory: 'build',
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    build: {
-        outDir: 'public/build',
-        manifest: true,
-        rollupOptions: {
-            input: ['resources/js/app.js'],
+  plugins: [
+    laravel({
+      input: ['resources/js/app.js'],
+      refresh: true,
+      buildDirectory: 'build',
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
         },
+      },
+    }),
+  ],
+  build: {
+    outDir: 'public/build',
+    manifest: true,
+    rollupOptions: {
+      input: ['resources/js/app.js'],
     },
-    server: {
-        https: true,
-        hmr: {
-            host: 'board.filkx.com',
-        },
+  },
+  server: {
+    https: true,
+    hmr: {
+      host: 'board.filkx.com',
     },
+  },
 });
