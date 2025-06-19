a70d353650d45dba5087c907f454473a90802bb5
Roma Volkov <Drakyla60@gmail.com>
2025-06-19 12:18:56 +0300

    add migrate boost

---
+++ database/migrations/2025_06_19_070145_create_advert_services_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('advert_services', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('advert_id')->constrained('advert_adverts')->onDelete('cascade');
+            $table->enum('type', ['highlight', 'pin', 'premium', 'urgent', 'boost']);
+            $table->timestamp('starts_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->integer('auto_ups_left')->nullable(); // для max-пакету
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('advert_services');
+    }
+};
