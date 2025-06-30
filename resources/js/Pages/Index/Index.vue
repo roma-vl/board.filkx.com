f7d9a4899c1679f681bc7f0a1b31550ccf4aa0f7
Roma Volkov <Drakyla60@gmail.com>
2025-06-30 21:27:39 +0300

    index components

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { usePage } from '@inertiajs/vue3';
+
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+import StaticSection from '@/Pages/Index/StaticSection.vue';
+import CookieBanner from '@/Components/CookieBanner.vue';
+import SearchBar from '@/Pages/Index/SearchBar.vue';
+import CategoriesGrid from '@/Pages/Index/CategoriesGrid.vue';
+import VipListings from '@/Pages/Index/VipListings.vue';
+import LatestListings from '@/Pages/Index/LatestListings.vue';
+
+const { categories, news, vip } = usePage().props;
+</script>
+
+<template>
+  <AuthenticatedLayout>
+    <div class="py-2">
+      <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div
+          class="overflow-hidden bg-white sm:rounded-3xl p-6 dark:bg-gray-700 dark:text-gray-100"
+        >
+          <div class="container mx-auto">
+            <SearchBar />
+            <CategoriesGrid :categories="categories" />
+            <VipListings :vip="vip" />
+            <LatestListings :news="news" />
+            <StaticSection />
+          </div>
+        </div>
+      </div>
+    </div>
+    <CookieBanner />
+  </AuthenticatedLayout>
+</template>
---
+++ resources/js/Pages/Index/VipListings.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ListingsGrid from './ListingsGrid.vue';
+const props = defineProps({
+  vip: Array,
+});
+</script>
+
+<template>
+  <section class="bg-white p-6 dark:bg-gray-800 rounded-md">
+    <h2 class="text-2xl font-bold mb-6 text-gray-800 dark:text-gray-300">
+      {{ $t('vip.announcement') }}
+    </h2>
+    <ListingsGrid :listings="vip" />
+  </section>
+</template>
