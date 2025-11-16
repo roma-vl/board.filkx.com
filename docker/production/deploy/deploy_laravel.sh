c78b1a3b9719ba73d1828214d37d4ec0e0a25d13
Roma Volkov <Drakyla60@gmail.com>
2025-11-16 03:04:43 +0200

    test build 28

--- docker/production/deploy/deploy_laravel.sh
+++ docker/production/deploy/deploy_laravel.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 COLOR=$1
 APP_DIR="/var/www/board.filkx.com"
 RELEASE_DIR="$APP_DIR/$COLOR"
-DOCKER_COMPOSE_FILE="$RELEASE_DIR/docker/production/docker-compose.yml"
+DOCKER_COMPOSE_FILE="$RELEASE_DIR/docker-compose-production.yml"
 WORKDIR_IN_CONTAINER="/var/www"
 
 # -----------------------------
